0d1c1ac890da7040ef4e2df7c3fba915aeb8113c
kedar-k <kedarkodgire@gmail.com>
2023-07-05 11:26:35 +0530

    added a question struct

---
+++ Quizzler-iOS13/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Kedar Kodgire on 05/07/23.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: String
+}
--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -15,12 +15,27 @@ class ViewController: UIViewController {
     @IBOutlet weak var progressBar: UIProgressView!
     @IBOutlet weak var questionLabel: UILabel!
     
+    let quiz = [
+                Question(text: "A slug's blood is green.", answer: "True"),
+                Question(text: "Approximately one quarter of human bones are in the feet.", answer: "True"),
+                Question(text: "The total surface area of two human lungs is approximately 70 square metres.", answer: "True"),
+        ]
+    
+    var questionNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateUI()
     }
 
-    @IBAction func answerButtonPressed(_ sender: Any) {
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        let userAnswer = sender.currentTitle
+        let actualAnswer = quiz[questionNumber].answer
+    }
+    
+    func updateUI(){
+        
     }
     
 }
